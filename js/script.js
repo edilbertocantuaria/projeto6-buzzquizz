5b708cb9e51a787565ad39ff348b889c12391043
ana-btz <silva.anabtz@gmail.com>
2022-11-04 08:39:56 -0300

    requisitos implementados: requisição axios de um quiz ao ser clicado, função que exibe o banner do quiz no topo da página (tela 2). mudanças: comentei ambos arquivos html dentro da pasta e inseri seus conteúdos no index.html

--- js/script.js
+++ js/script.js
@@ -1,31 +1,46 @@
+// ======================== VARIAVEIS GLOBAIS ======================== 
+
 // criar var globais, uma para os quizzes do usuario e outra para os demais quizzes
-// obs: nao sei se serao necessarias, mas deixai ai :/ 
-let quizzesUsuario;
-let quizzesServidor;
+// OBS: nao sei se serao necessarias, mas deixei ai (ass: ana)
+let quizzesUsuario = [];
+let quizzesServidor = [];
+
+// armazenar TODOS os quizzes trazidos do servidor
+let quizzesData;
+
+// armazenar APENAS os dados do quizz selecionado
+let quizzData;
 
 // armazenar os ids no momento da criação dos quizzes
-const listaIds = [];
+let listaIds = [];
 const listaIdsSerializada = JSON.stringify(listaIds);
-localStorage.setItem("id", listaIdsSerializada);
+localStorage.setItem("id-user", listaIdsSerializada);
+
+// armazenar os ids dos demais quizzes capturados do servidor
+let listaIdsServidor = [];
+
+// armazenar o id do quizz que foi selecionado
+let idSelecionado;
+
+// ==================== TELA 1: LISTA DE QUIZZES =====================
 
 // requisicao axios --> buscar todos os quizzes
 function buscaQuizzes() {
     const promise = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
     // console.log(promise);
     promise.then(renderizarQuizzes);
-    promise.catch();
 }
 
 function renderizarQuizzes(resposta) {
     // console.log(resposta);
-    const quizzesData = resposta.data;
-    console.log(quizzesData);
+    quizzesData = resposta.data;
+    // console.log(quizzesData);
     const elementoUserList = document.querySelector(".user-list");
     const elementoServerList = document.querySelector(".server-list");
     // console.log(elementoUserList);
     // console.log(elementoServerList);
 
-    // elementoUserList.innerHTML = "";
+    elementoUserList.innerHTML = "";
     elementoServerList.innerHTML = "";
 
     // pegar de volta os ids armazenados
@@ -42,50 +57,115 @@ function renderizarQuizzes(resposta) {
             for (let j = 0; j < ids.length; j++) {
                 if (quizzesData[i].id === ids[j]) {
                     quizz = `
-                        <li class="quizz">
+                        <li class="my-list-quizz ${quizzesData[i].id}" onclick="selecionaQuizz(this)">
                             <img src=${quizzesData[i].image}>
                             <p>${quizzesData[i].title}</p>
                         </li>
                     `
+                    // armazenar objeto quizz na lista
+                    quizzesUsuario.push(quizzesData[i]);
+
                     elementoUserList.innerHTML += quizz;
                 } else {
                     quizz = `
-                        <li class="quizz">
+                        <li class="quizz ${quizzesData[i].id}" onclick="selecionaQuizz(this)">
                             <img src=${quizzesData[i].image}>
                             <p>${quizzesData[i].title}</p>
                         </li>
                     `
+                    // armazenar objeto quizz na lista
+                    quizzesServidor.push(quizzesData[i]);
+
+                    // armazenar o id do quizz na lista
+                    listaIdsServidor.push(quizzesData[i].id);
+
                     elementoServerList.innerHTML += quizz;
                     console.log(quizz);
                 }
             }
         } else {
             quizz = `
-                <li class="quizz">
+                <li class="quizz ${quizzesData[i].id}" onclick="selecionaQuizz(this)">
                     <img src=${quizzesData[i].image}>
                     <p>${quizzesData[i].title}</p>
                 </li>
             `
+            // armazenar objeto quizz na lista
+            quizzesServidor.push(quizzesData[i]);
+
+            // armazenar o id do quizz na lista
+            listaIdsServidor.push(quizzesData[i].id);
+
             elementoServerList.innerHTML += quizz;
             console.log(quizz);
         }
     }
 }
-function criarQuizz(){
+
+// funcao executada quando algum quizz e clicado
+function selecionaQuizz(quizzSelecionado) {
+    quizzSelecionado.classList.add("selecionado");
+    // console.log(quizzSelecionado);
+
+    for (let k = 0; k < quizzesData.length; k++) {
+        const classeId = quizzesData[k].id;
+
+        if (quizzSelecionado.classList.contains(classeId.toString())) {
+            idSelecionado = classeId;
+        }
+        // nao sei o que por no else :(
+    }
+
+    // clicar em um quizz na tela 1 --> redirecionar para tela 2 
+    // window.location.replace("./pages/quizz.html");
+    buscaQuizz();
+}
+
+// ================ TELA 2: PÁGINA DE UM QUIZZ (PERGUNTAS) ================
+
+// requisicao axios --> buscar dados do quiz clicado
+function buscaQuizz() {
+    const promise = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${idSelecionado}`);
+    promise.then(tratarSucesso);
+}
+
+function tratarSucesso(resposta) {
+    console.log(resposta);
+    quizzData = resposta.data;
+    console.log(quizzData);
+
+    renderizarBaner();
+    // renderizarPerguntas();
+}
+
+function renderizarBaner() {
+    const elementoBaner = document.querySelector(".titulo");
+    const imgBaner = quizzData.image;
+    const titulo = quizzData.title;
+
+    elementoBaner.innerHTML = "";
+
+    elementoBaner.innerHTML += `
+        <img src=${imgBaner}>
+        <h2>${titulo}</h2>
+    `
+}
+
+function criarQuizz() {
     const main = document.querySelector('.main');
     const page02 = document.querySelector('page02');
     main.classList.remove('main');
     main.classList.add('escondido');
     page02.classList.add('main-criandoQuizz');
 }
-function btnProssseguir(){
+function btnProssseguir() {
     const criandoQuizz = document.querySelector('.section-quizz');
     const criarPerguntas = document.querySelector('.section01');
     criandoQuizz.classList.remove('section-quizz');
     criarPerguntas.classList.remove('escondido');
     criarPerguntas.classList.add('section-quizz');
 }
-function btnProsseguir2(){
+function btnProsseguir2() {
     const criarPerguntas = document.querySelector('.section01');
     const section02 = document.querySelector('.section02');
     criarPerguntas.classList.remove('section-quizz');
@@ -93,7 +173,7 @@ function btnProsseguir2(){
     section02.classList.remove('escondio');
     section02.classList.add('section-niveis');
 }
-function btnFinalizarQuizz(){
+function btnFinalizarQuizz() {
     const section02 = document.querySelector('.section02');
     const section03 = document.querySelector('.section03');
     section02.classList.remove('section-niveis');
@@ -101,7 +181,8 @@ function btnFinalizarQuizz(){
     section03.classList.remove('escondido');
     section03.classList.add('section-finalizer-Quizz');
 }
-function voltarHome(){
-    window.location.replace("../pages/page01.html");
+function voltarHome() {
+    window.location.replace("index.html");
+
 }
 buscaQuizzes();
